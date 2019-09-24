f5199e7833ea737f0dc492c9bc696abfb3943959
TEPPEI KAWASHIMA <teppei@TEPPEInoMBP.lan>
2019-09-24 22:17:32 +0900

    uploaded files

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  root 'static_pages#top'
+
+  resources :user_sessions, only: %i[new create destroy]
+  resources :users, only: %i[new create]
+
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
+end
